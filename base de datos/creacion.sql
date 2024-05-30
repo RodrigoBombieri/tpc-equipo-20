a96a1171e415852b9003e62c6998366f86da0846
thecloudbuster <francocataldo7@gmail.com>
2024-05-30 12:10:28 -0300

    modifique tabla domicilio y cliente en db

--- base de datos/creacion.sql
+++ base de datos/creacion.sql
@@ -2,12 +2,34 @@ Create Database TPC
 Go
 Use TPC
 Go
+Create Table Paises (
+	ID smallint not null primary key identity (1, 1),
+	Nombre varchar(100) not null
+)
+Go
+Create Table Localidades(
+	ID smallint not null primary key identity (1, 1),
+	Nombre varchar(100) not null,
+	IDPais smallint not null foreign key references Paises(ID),
+)
+Go
+Create Table Domicilios(
+    ID bigint not null primary key identity (1,1),
+    Calle varchar(50) not null,
+	Numero smallint not null,
+	Piso tinyint not null,
+	Departamento tinyint not null,
+	Observaciones varchar(200) not null,
+	IDLocalidad smallint not null foreign key references Localidades(ID),
+)
+Go
 Create Table Clientes(
 	ID bigint not null primary key identity (1, 1),
 	Nombre varchar(100) not null,
 	Apellido varchar(100) not null,
 	Dni varchar(15) not null unique,
 	Telefono varchar(30) null,
+	IDDomicilio bigint not null foreign key references Domicilios(ID),
 	Email varchar(50) not null unique,
 	FechaNacimiento date null,
 	FechaCreacion date not null
@@ -28,27 +50,6 @@ Create Table Usuarios(
 	Email varchar(50) not null unique,
 	Pass varchar(100) not null,
 	IDRol int not null foreign key references Roles(ID),
-)
-Go
-Create Table Paises (
-	ID smallint not null primary key identity (1, 1),
-	Nombre varchar(100) not null
-)
-Go
-Create Table Localidades(
-	ID smallint not null primary key identity (1, 1),
-	Nombre varchar(100) not null,
-	IDPais smallint not null foreign key references Paises(ID),
-)
-Go
-Create Table Domicilios(
-    ID bigint not null primary key foreign key references Clientes(ID),
-    Calle varchar(50) not null,
-	Numero smallint not null,
-	Piso tinyint not null,
-	Departamento tinyint not null,
-	Observaciones varchar(200) not null,
-	IDLocalidad smallint not null foreign key references Localidades(ID),
 )
 Go
 Create Table Estados(
@@ -84,3 +85,6 @@ insert into TIPOSINCIDENTES values ('Producto dañado'), ('Problema en el cobro'
 
 
 select * from ROLES
+select * from ESTADOS
+select * from PRIORIDADES
+select * from TIPOSINCIDENTES
