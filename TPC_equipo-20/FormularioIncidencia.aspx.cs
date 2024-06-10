90b30177c74cbdd47f6c490a30b012a919321416
lucas <lucas.saputo@gmail.com>
2024-06-09 21:00:17 -0300

    "home" de incidencias, front mas que nada incluido filtros. Formulario incidencias separado

---
+++ TPC_equipo-20/FormularioIncidencia.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TPC_equipo_20
+{
+    public partial class FormularioIncidencia : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            ddlPrioridad.Items.Add("Baja");
+            ddlPrioridad.Items.Add("Media");
+            ddlPrioridad.Items.Add("Alta");
+            ddlEstado.Items.Add("1");
+            ddlEstado.Items.Add("2");
+            ddlEstado.Items.Add("3");
+        }
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
